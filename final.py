d69d3486d2071f448e04343466b0d4178c60b1b7
Mrinmoy Saha Roddur <mrinmoy.roddur@gmail.com>
2020-09-21 06:04:39 +0600

    final this time

--- final.py
+++ final.py
@@ -44,12 +44,21 @@ m1.append((data_m1.iloc[3]/(data_m1.iloc[0]-3),data_m1.iloc[4]/(data_m1.iloc[0]-
 data_m2=data.loc[data.iloc[:,0]=='1000M4'].iloc[:,2:].mean(axis=0)
 m2.append((data_m2.iloc[3]/(data_m2.iloc[0]-3),data_m2.iloc[4]/(data_m2.iloc[0]-3), data_m2.iloc[5]))
 
+
+for i in range(4):
+    print(al[i])
+    print(m1[i])
+    print(m2[i])
+
+
+
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 N = 4
 
-'''
+
 fp = [i[0] for i in al]
 fn = [i[1] for i in al]
 
@@ -82,7 +91,7 @@ plt.title('Average FN/FP rate for 1000M1 dataset')
 plt.xticks(ind + width / 2, ('FastTree(JC69)', 'FastTree(GTR)', 'NJ(JC distance)', 'NJ(Logdet)'))
 plt.legend(loc='best')
 plt.savefig('m1.png')
-'''
+
 
 fp = [i[0] for i in m2]
 fn = [i[1] for i in m2]
@@ -99,3 +108,4 @@ plt.title('Average FN/FP rate for 1000M4 dataset')
 plt.xticks(ind + width / 2, ('FastTree(JC69)', 'FastTree(GTR)', 'NJ(JC distance)', 'NJ(Logdet)'))
 plt.legend(loc='best')
 plt.savefig('m2.png')
+'''
--- script.sh
+++ script.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-FILES="1000M[1-9]/R*"
+FILES="1000M[1-2]/R*"
 
 for i in $FILES
 do
     start=`date +%s%N`
-    #python3 fasta2phylip.py $i/rose.aln.true.fasta $i/rose.aln.true.phylip
-    #./FastTree -nt $i/rose.aln.true.fasta > $i/FastTree_GTR.et
-    #./FastTree -nt -gtr $i/rose.aln.true.fasta > $i/FastTree_GTR.et
-    rm $i/NJ_L.et
+    ./FastTree -nt $i/rose.aln.true.fasta > $i/FastTree_JC.et
+    end=`date +%s%N`
+    python3 compare.py $i/rose.tt $i/NJ_L.et >> FastTree_JC.csv
+    echo $(bc <<< "scale=2;($end - $start)/1000000000" ) >> FastTree_JC.csv
+
+    start=`date +%s%N`
+    ./FastTree -nt -gtr $i/rose.aln.true.fasta > $i/FastTree_GTR.et
+    end=`date +%s%N`
+    python3 compare.py $i/rose.tt $i/NJ_L.et >> FastTree_GTR.csv
+    echo $(bc <<< "scale=2;($end - $start)/1000000000" ) >> FastTree_GTR.csv
+
+    python3 fasta2phylip.py $i/rose.aln.true.fasta $i/rose.aln.true.phylip
+    start=`date +%s%N`
+    ./fastme-2.1.5-linux64 -mN -dJ -i $i/rose.aln.true.phylip -o $i/NJ_JC.et
+    end=`date +%s%N`
+    python3 compare.py $i/rose.tt $i/NJ_L.et >> NJ_JC.csv
+    echo $(bc <<< "scale=2;($end - $start)/1000000000" ) >> NJ_JC.csv
+
+    start=`date +%s%N`
     ./fastme-2.1.5-linux64 -mN -dL -i $i/rose.aln.true.phylip -o $i/NJ_L.et
     end=`date +%s%N`
     python3 compare.py $i/rose.tt $i/NJ_L.et >> NJ_L.csv
     echo $(bc <<< "scale=2;($end - $start)/1000000000" ) >> NJ_L.csv
-    echo "------------------------------------------------------------------------------------------------------"
+
 done
 
-
-#./FastTree -nt 1000M1/R0/rose.aln.true.fasta > testtree.txt
+python3 final.py
